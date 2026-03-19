2bc77ddc29759312053739ddc5f4b6e5bc0a3e42
usmanahad <112814349+usmanahad@users.noreply.github.com>
2026-03-19 15:07:16 +0500

    final failed build

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -11,4 +11,23 @@ public class CustomListTest {
         // This will fail initially because hasCity() doesn't exist
         assertTrue(list.hasCity(calgary));
     }
+
+    @Test
+    public void testDeleteCity(){
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        list.deleteCity(calgary);
+        assertFalse(list.hasCity(calgary));
+    }
+
+    @Test
+    public void testCountCities(){
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        City toronto = new City("Toronto", "ON");
+        list.addCity(calgary);
+        list.addCity(toronto);
+        assertEquals(2, list.countCities());
+    }
 }
